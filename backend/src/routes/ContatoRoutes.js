ba923ba5279fe9f29ae9222b7779b8460b9df7d5
premix-oficiall <premix.info.br@gmail.com>
2025-10-10 10:50:30 -0300

    Add files via upload

---
+++ backend/src/routes/ContatoRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const contatoController = require('../controllers/ContatoController');
+
+// POST - Criar novo contato
+router.post('/', contatoController.criarContato);
+
+// GET - Listar todos os contatos (com filtros opcionais)
+// Query params opcionais: ?status=pendente&empresa=Premix&dataInicio=2025-01-01&dataFim=2025-12-31
+router.get('/', contatoController.listarContatos);
+
+// GET - Estatísticas de contatos
+router.get('/estatisticas', contatoController.estatisticas);
+
+// GET - Buscar contato por ID
+router.get('/:id', contatoController.buscarContatoPorId);
+
+// PATCH - Atualizar status do contato
+router.patch('/:id/status', contatoController.atualizarStatus);
+
+// DELETE - Deletar contato
+router.delete('/:id', contatoController.deletarContato);
+
+module.exports = router;
